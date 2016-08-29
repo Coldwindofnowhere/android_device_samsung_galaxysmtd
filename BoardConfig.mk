1f4422b05b9dd77caa010f7577bb7f9fb0da7936
Coldwindofnowhere <coldwindofnowhere@gmail.com>
2016-08-29 19:03:47 +0200

    AOSP 7.0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,6 @@
 #
-# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2013 OmniROM Project
+# Copyright (C) 2007 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,11 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This variable is set first, so it can be overridden
-# by BoardConfigVendor.mk
+# BoardConfig.mk
+#
+# Product-specific compile-time definitions.
+#
 
 #Video Devices
 BOARD_SECOND_CAMERA_DEVICE := /dev/video2
+
+# Kernel Config
+TARGET_KERNEL_CONFIG := omni_galaxysmtd_defconfig
 
 # FM Radio
 BOARD_HAVE_FM_RADIO := true
@@ -27,14 +33,10 @@ BOARD_FM_DEVICE := si4709
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/galaxysmtd/bluetooth
 
-# Inline kernel building
-TARGET_KERNEL_SOURCE := kernel/samsung/aries
-TARGET_KERNEL_CONFIG := omni_galaxysmtd_defconfig
-
 # Recovery
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxysmtd/recovery/recovery_keys.c
 
 TARGET_OTA_ASSERT_DEVICE := galaxys,galaxysmtd,GT-I9000,GT-I9000M,GT-I9000T
 
-# Use the non-open-source parts, if they're present
+# Import the aries-common BoardConfigCommon.mk
 include device/samsung/aries-common/BoardConfigCommon.mk
---
+++ aosp_galaxysmtd.mk
@@ -0,0 +1,36 @@
+# Copyright (C) 2013 OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# low ram device
+TARGET_LOW_RAM_DEVICE := true
+
+# Get the long list of APNs
+#PRODUCT_COPY_FILES := device/samsung/aries-common/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/galaxysmtd/device.mk)
+
+# Device identifier
+PRODUCT_RELEASE_NAME := GalaxyS
+PRODUCT_DEVICE := galaxysmtd
+PRODUCT_NAME := aosp_galaxysmtd
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-I9000
+
+# Build Launcher
+PRODUCT_PACKAGES += \
+	Launcher3
